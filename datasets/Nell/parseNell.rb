bef3f65832e294ea949ff6eace51573fa407ae9a
Eriq Augustine <eriq.augustine@gmail.com>
2017-01-04 20:56:05 -0800

    Changed up the Nell parser to optimize for low memory usage so it can be done on any(ish) machine.

--- datasets/Nell/parseNell.rb
+++ datasets/Nell/parseNell.rb
@@ -1,10 +1,15 @@
 require 'fileutils'
 require 'uri'
 
+# The full set of Nell files is pretty huge.
+# Even holding a single column in memory can be tens of gigs.
+# So, we will be optimizing for low memory usage at the cost of speed.
+# We will let the DB handle de-duplication.
+
 SKIP_FIRST_LINE = true
 
 # Batch the inserts to we don't run out of memory.
-MAX_INSERTS = 10000
+PAGE_SIZE = 10000
 
 INSERT_DIR = File.join('sql', 'insert')
 ENTITIES_FILE = File.join(INSERT_DIR, 'entities.sql')
@@ -39,9 +44,11 @@ COL_TYPE_INT = 'int'
 COL_TYPE_FLOAT = 'float'
 COL_TYPE_BOOL = 'bool'
 
-# [{valueIndex => value, ...}, ...] will be returned.
+# Call |block| with one parameter: [[col1, col2, ...], ...]
 # One hash per line.
-def fetchValues(path, indexes)
+# There will be at most PAGE_SIZE values in the array passed to the block.
+# The block will never get called on an empty hash.
+def fetchValues(path, &block)
    values = []
    first = true
 
@@ -53,16 +60,22 @@ def fetchValues(path, indexes)
          end
 
          # Make sure not to strip first just in case there are empty files at the end.
-         parts = line.split("\t").map{|val| val.strip()}
+         values << line.split("\t").map{|val| val.strip()}
 
-         values << indexes.map{|valueIndex| [valueIndex, parts[valueIndex]]}.to_h()
+         if (values.size() == PAGE_SIZE)
+            block.call(values)
+            values.clear()
+         end
       }
-   }
 
-   return values
+      if (values.size() != 0)
+         block.call(values)
+         values.clear()
+      end
+   }
 end
 
-# TODO(eriq)
+# TODO(eriq): Do better
 def escape(string)
    return string.gsub("'", "''")
 end
@@ -102,27 +115,31 @@ end
 # |rowInfo| = [{:name => someName, :options => {...}}, ...]
 #  :options is passed blindly to prepValue().
 def writeInsert(path, tableName, rows, rowInfo)
+   if (rows.size() == 0)
+      return
+   end
+
    header = ''
    header += "INSERT INTO #{tableName}\n"
    header += "   (#{rowInfo.map{|row| row[:name]}.join(", ")})\n"
    header += 'VALUES'
 
-   File.open(path, 'w'){|file|
-      rows.each_slice(MAX_INSERTS){|insertRows|
-         valuesString = insertRows.map{|row| "   (#{row.map.with_index{|val, i| prepValue(val, rowInfo[i][:options])}.join(", ")})"}.join(",\n")
+   File.open(path, 'a'){|file|
+      valuesString = rows.map{|row| "   (#{row.map.with_index{|val, i| prepValue(val, rowInfo[i][:options])}.join(", ")})"}.join(",\n")
 
-         file.puts(header)
-         file.puts(valuesString)
-         file.puts(";\n")
-      }
+      file.puts(header)
+      file.puts(valuesString)
+      file.puts('ON CONFLICT DO NOTHING')
+      file.puts(";\n")
    }
 end
 
-def parseTriples(path)
+def parseTriples(path, rawRows)
    columns = [ENTITY, RELATION, VALUE, ITERATION_OF_PROMOTION, PROBABILITY, SOURCE, CANDIDATE_SOURCE]
-   rows = fetchValues(path, columns).map{|val| columns.map{|key| val[key]}}
+   rows = rawRows.map{|val| columns.map{|key| val[key]}}
 
-   # Costly, but necessary...
+   # Costly, but usually worth it.
+   # Even triples have dups in Nell.
    rows.uniq!
 
    writeInsert(TRIPLES_FILE, TRIPLES_TABLE, rows, [
@@ -136,24 +153,20 @@ def parseTriples(path)
    ])
 end
 
-def parseRelations(path)
-   rows = fetchValues(path, [RELATION]).map{|val| [val[RELATION]]}
+def parseRelations(path, rawRows)
+   rows = rawRows.map{|val| [val[RELATION]]}
 
-   # Costly, but necessary...
+   # Costly, but usually worth it.
    rows.uniq!
 
    writeInsert(RELATIONS_FILE, RELATIONS_TABLE, rows, [{:name => 'nellId'}])
 end
 
-def parseEntities(path)
-   rawRows = fetchValues(path, [ENTITY, VALUE])
-   
+def parseEntities(path, rawRows)
    rows  = rawRows.map{|rawRow| [rawRow[ENTITY], true]}
    rows += rawRows.map{|rawRow| [rawRow[VALUE], rawRow[VALUE].start_with?('concept:')]}
 
-   rawRows = nil
-
-   # Costly, but necessary...
+   # Costly, but usually worth it.
    rows.uniq!
 
    writeInsert(ENTITIES_FILE, ENTITIES_TABLE, rows, [
@@ -162,54 +175,51 @@ def parseEntities(path)
    ])
 end
 
-def parseLiterals(path)
-   rawRows = fetchValues(path, [
-      ENTITY, ENTITY_LITERALSTRINGS, BEST_ENTITY_LITERALSTRING,
-      VALUE, VALUE_LITERALSTRINGS, BEST_VALUE_LITERALSTRING
-   ])
-
+def parseLiterals(path, rawRows)
    rows  = rawRows.map{|rawRow| [rawRow[ENTITY], rawRow[ENTITY_LITERALSTRINGS], rawRow[BEST_ENTITY_LITERALSTRING]]}
    rows += rawRows.map{|rawRow| [rawRow[VALUE], rawRow[VALUE_LITERALSTRINGS], rawRow[BEST_VALUE_LITERALSTRING]]}
-
-   # Clean it up plz.
-   rawRows = nil
 
    # Remove empty values.
    rows.reject!{|row| row[1] == '' && row[2] == ''}
 
-   # Costly, but necessary...
+   # Costly, but usually worth it.
    rows.uniq!
 
    writeInsert(LITERALS_FILE, LITERALS_TABLE, rows, [{:name => 'entityNellId'}, {:name => 'literal'}, {:name => 'bestLiteral'}])
 end
 
-def parseCategories(path)
-   rawRows = fetchValues(path, [ENTITY, CATEGORIES_FOR_ENTITY, VALUE, CATEGORIES_FOR_VALUE])
-
+def parseCategories(path, rawRows)
    rows  = rawRows.map{|rawRow| [rawRow[ENTITY], rawRow[CATEGORIES_FOR_ENTITY]]}
    rows += rawRows.map{|rawRow| [rawRow[VALUE], rawRow[CATEGORIES_FOR_VALUE]]}
-
-   # Clean it up plz.
-   rawRows = nil
 
    # Remove empty values.
    rows.reject!{|row| row[1] == ''}
 
-   # Costly, but necessary...
+   # Costly, but usually worth it.
    rows.uniq!
 
    writeInsert(CATEGORIES_FILE, CATEGORIES_TABLE, rows, [{:name => 'entityNellId'}, {:name => 'category'}])
 end
 
+def removeOldFiles()
+   toRemove = [ENTITIES_FILE, RELATIONS_FILE, LITERALS_FILE, CATEGORIES_FILE, TRIPLES_FILE]
+   toRemove.each{|path|
+      FileUtils.rm(path)
+   }
+end
+
 def parseFile(path)
    FileUtils.mkdir_p(INSERT_DIR)
 
-   # To save memory, we are only going to fetch specific values from the file at a time.
-   parseEntities(path)
-   parseLiterals(path)
-   parseCategories(path)
-   parseRelations(path)
-   parseTriples(path)
+   removeOldFiles()
+
+   fetchValues(path){|rawRows|
+      parseEntities(path, rawRows)
+      parseLiterals(path, rawRows)
+      parseCategories(path, rawRows)
+      parseRelations(path, rawRows)
+      parseTriples(path, rawRows)
+   }
 end
 
 def main(args)
